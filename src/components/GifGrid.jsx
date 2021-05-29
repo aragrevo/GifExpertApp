b0bcfc487fe9a1462bc285ade756263971426cb3
aragrevo <eduver_san@hotmail.com>
2021-05-29 11:46:17 -0500

    Loading

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import useFetchGifs from '../hooks/useFetchGifs';
 import GifGridItem from './GifGridItem';
+import Loading from './Loading';
 
 const GifGrid = ({ category }) => {
   const { data: gifs, loading } = useFetchGifs(category);
@@ -10,7 +11,7 @@ const GifGrid = ({ category }) => {
       <div className='section-title'>
         <h3>{category}</h3>
       </div>
-      {loading && <p>Loading</p>}
+      {loading && <Loading />}
       <div className='row'>
         {gifs.map((gif) => (
           <GifGridItem key={gif.id} {...gif} />
